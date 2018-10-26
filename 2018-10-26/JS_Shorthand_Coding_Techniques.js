1f94f9fb1de0d728d114a9b1b1c2123cebbedd56
Shokr <mohammedshokr2014@gmail.com>
2018-10-26 13:57:52 +0200

    [ 12 ]  Multi-line String Shorthand

--- 2018-10-26/JS_Shorthand_Coding_Techniques.js
+++ 2018-10-26/JS_Shorthand_Coding_Techniques.js
@@ -105,3 +105,14 @@ last = "Daily"
 const welcome = `You have logged in as ${first} ${last}`;
 
 console.log(welcome);
+
+
+// [ 12 ]  Multi-line String Shorthand
+const lorem = `Lorem ipsum dolor sit amet, consectetur
+    adipisicing elit, sed do eiusmod tempor incididunt
+    ut labore et dolore magna aliqua. Ut enim ad minim
+    veniam, quis nostrud exercitation ullamco laboris
+    nisi ut aliquip ex ea commodo consequat. Duis aute
+    irure dolor in reprehenderit in voluptate velit esse.`
+
+console.log(lorem);
