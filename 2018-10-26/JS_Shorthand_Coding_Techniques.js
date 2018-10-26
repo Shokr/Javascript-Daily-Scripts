a5428858d0689295adc594ce05d891422262c12c
Shokr <mohammedshokr2014@gmail.com>
2018-10-26 12:59:32 +0200

    [ 1 ] The Ternary Operator :: if..else statement in just one line.

---
+++ 2018-10-26/JS_Shorthand_Coding_Techniques.js
@@ -0,0 +1,17 @@
+/**
+ * 19 + JavaScript Shorthand Coding Techniques
+ * https: //www.sitepoint.com/shorthand-javascript-techniques/
+ * 
+ */
+
+ console.log("==> JavaScript Shorthand Coding Techniques  <==");
+
+ // [ 1 ] The Ternary Operator :: if..else statement in just one line.
+ 
+ let x = 20;
+ const answer = x > 10 ? 'greater than 10' : 'less than 10';
+
+ // output
+ console.log("{1} (const answer = x > 10 ? 'greater than 10' : 'less than 10';) ");
+ console.log("result:: "+ answer);
+
